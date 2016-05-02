bf76b1a0867f4c20f59b9724559f2092ba87403d
chaiery <yhmyhm@mail.ustc.edu.cn>
2016-05-02 23:27:06 +0800

    update config

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ CLIENT_ID = 'f1048249-2603-4706-affd-97ef8c0c4495'
 REDIRECT_URI = 'http://localhost:8000/recv_redirect'
 
 SCOPES = ['user/Sequence.read',
-          'user/Sequence.write',
         'user/observationforgenetics.read',
         'user/Patient.read'
 ]
